a882bcac3165f10e6111978ffbda42cb14dfcec2
DanyaSh <danilashundeev@gmail.com>
2023-06-29 15:33:53 -0300

    begin git

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask, render_template, url_for
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+menu=["Установка", "Первое приложение", "Обратная связь"]
+
+@app.route('/')
+def index():
+    print(url_for("index"))
+    return render_template('index.html', menu=menu)
+
+@app.route('/about')
+def about():
+    print(url_for("about"))
+    return render_template('about.html', title="О сайте", menu=menu)
+
+if __name__=="__main__":
+    app.run(debug=True)
