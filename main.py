10bb0eccfdb8b46bbc68db9cb5ec1af378cf4088
DanyaSh <danilashundeev@gmail.com>
2023-07-06 19:37:05 -0300

    flash end

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ code - flash
 '''
 
 import os
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, flash
 from dotenv import load_dotenv
 
 load_dotenv('.env')
 
 app = Flask(__name__)
 app.config['DATA_TEXT'] = os.getenv('DATA_TEXT')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 menu=[{'name':"Установка", 'url':'install-flask'},
       {'name':"Первое приложение", 'url':'first-app'},
@@ -29,6 +30,10 @@ def about():
 @app.route('/contact', methods=["POST", "GET"])
 def contact():
     if request.method=="POST":
+        if len(request.form['username'])>2:
+            flash("Сообщение отправлено", category="success")
+        else:
+            flash("Ошибка отправки", category="error")
         print(request.form)
         print(request.form['username'])
     return render_template('contact.html', title="Обратная связь", menu=menu)
