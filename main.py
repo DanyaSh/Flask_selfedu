0a00014be853e0523acffb9acde922b7387a73f2
DanyaSh <danilashundeev@gmail.com>
2023-07-07 21:58:30 -0300

    login

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 '''
-Lesson 06
-code - flash
+Lesson 07
+code - login
 '''
 
 import os
-from flask import Flask, render_template, url_for, request, flash
+from flask import Flask, render_template, url_for, request, flash, session, redirect, abort
 from dotenv import load_dotenv
 
 load_dotenv('.env')
@@ -27,6 +27,16 @@ def about():
     print(url_for("about"))
     return render_template('about.html', title="О сайте", menu=menu)
 
+@app.route('/login', methods=["POST", "GET"])
+def login():
+    print(url_for("login"))
+    if 'userLogged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.method=="POST" and request.form['username']=='danyash' and request.form['pswd']=='123':
+        session['userLogged']=request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+    return render_template('login.html', title="Авторизация", menu=menu)
+
 @app.route('/contact', methods=["POST", "GET"])
 def contact():
     if request.method=="POST":
@@ -41,7 +51,13 @@ def contact():
 @app.route('/profile/<path:username>')
 def profile(username):
     print(url_for("profile", username=username))
+    if 'userLogged' not in session or session['userLogged']!=username:
+        abort(401)
     return f"Пользователь: {username}"
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page404.html', title="Страница не найдена", menu=menu), 404
 
 # with app.test_request_context():
 #     print(url_for("index"))
